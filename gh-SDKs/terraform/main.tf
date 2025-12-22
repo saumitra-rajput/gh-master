b0790f126163a87158fc0679a5abe6580d407747
Saumitra-Rajput <saumitrarajput3jan@gmail.com>
2025-12-22 09:26:45 +0000

    Created gh-SDK/terraform

---
+++ gh-SDKs/terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {}
+
+
+
+resource "github_branch" "development" {
+  repository = "GH-Basics"
+  branch     = "SDKs"
+}
